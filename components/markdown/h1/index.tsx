faab3035e13cbd34ae91d3ce46b0b91d92a3258f
Nishan Bende <nishanbende@gmail.com>
2021-08-18 23:04:39 +0530

    feat: add components

---
+++ components/markdown/h1/index.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const H1 = (props: any) => <h1 style={{ color: "tomato" }} {...props} />;
---
+++ components/markdown/index.ts
@@ -0,0 +1,5 @@
+import { H1 } from "./h1";
+
+export const components = {
+  h1: H1,
+};
